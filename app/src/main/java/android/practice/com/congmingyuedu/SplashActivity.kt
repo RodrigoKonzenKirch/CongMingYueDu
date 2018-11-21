2be5e5dcb5a5e47981f3af56df2fcfeb3f68075e
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2018-11-21 14:57:08 -0200

    Added splash screen

---
+++ app/src/main/java/android/practice/com/congmingyuedu/SplashActivity.kt
@@ -0,0 +1,18 @@
+package android.practice.com.congmingyuedu
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.practice.com.congmingyuedu.view.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+        //TODO: Update the splash screen's placeholder image with the final App's image
+    }
+}
--- app/src/main/java/android/practice/com/congmingyuedu/view/MainActivity.kt
+++ app/src/main/java/android/practice/com/congmingyuedu/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package android.practice.com.congmingyuedu
+package android.practice.com.congmingyuedu.view
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.practice.com.congmingyuedu.R
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
