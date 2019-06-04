cd99306506ce3cacf047f5b097c521bcc7c373e9
RodrigoKonzenKirch <rodrigokirch@gmail.com>
2019-06-04 04:25:04 -0300

    Deleted unneeded UI elements. Navigation is going to be implemented.

--- app/src/main/java/android/practice/com/congmingyuedu/view/MainActivity.kt
+++ app/src/main/java/android/practice/com/congmingyuedu/view/MainActivity.kt
@@ -2,21 +2,42 @@ package android.practice.com.congmingyuedu.view
 
 import android.os.Bundle
 import android.practice.com.congmingyuedu.R
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mDrawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragmentAdapter = MyPagerAdapter(supportFragmentManager)
-        viewpager_main.adapter = fragmentAdapter
-        viewpager_main.setSwipeLocked(true)
+        mDrawerLayout = findViewById(R.id.main_drawer_layout)
 
-        tabs_main.setupWithViewPager(viewpager_main)
-        //TODO: Implement click event on navigation drawer items
+        val navigationView: NavigationView = findViewById(R.id.main_nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            mDrawerLayout.closeDrawers()
+
+            //TODO: Implement click event on navigation drawer items
+            when(menuItem.itemId){
+                R.id.nav_read_text -> Toast.makeText(this, "read not implemented yet", Toast.LENGTH_LONG).show()
+                R.id.nav_open_text -> Toast.makeText(this, "open not implemented yet", Toast.LENGTH_LONG).show()
+                R.id.nav_import_text -> Toast.makeText(this, "import not implemented yet", Toast.LENGTH_LONG).show()
+                R.id.nav_delete_text -> Toast.makeText(this, "delete not implemented yet", Toast.LENGTH_LONG).show()
+                else -> Toast.makeText(this, "ELSE", Toast.LENGTH_LONG).show()
+
+            }
+
+            true
+        }
+
+
     }
 
 }
