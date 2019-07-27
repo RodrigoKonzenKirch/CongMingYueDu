17e6726a98d900bc4667e4f926c31b85fc3e5459
RodrigoKonzenKirch <rodrigokirch@gmail.com>
2019-07-27 01:38:13 -0300

    Populated database and added getCount and insertAll to dictionaryDao

--- app/src/main/java/android/practice/com/congmingyuedu/model/ChineseDictionaryDao.kt
+++ app/src/main/java/android/practice/com/congmingyuedu/model/ChineseDictionaryDao.kt
@@ -10,7 +10,14 @@ interface ChineseDictionaryDao {
     @Query("SELECT * FROM chinese_dictionary")
     fun getAll(): List<ChineseDictionary>
 
+    @Query("SELECT COUNT(id) FROM chinese_dictionary")
+    fun getCountDictionary(): Int
+
     @Insert
     fun insert(chineseDictionary: ChineseDictionary)
+
+    @Insert
+    fun insertAll(chineseDictionary: List<ChineseDictionary>)
+
 
 }
--- app/src/main/java/android/practice/com/congmingyuedu/view/MainActivity.kt
+++ app/src/main/java/android/practice/com/congmingyuedu/view/MainActivity.kt
@@ -2,6 +2,8 @@ package android.practice.com.congmingyuedu.view
 
 import android.os.Bundle
 import android.practice.com.congmingyuedu.R
+import android.practice.com.congmingyuedu.model.AppDatabase
+import android.practice.com.congmingyuedu.model.ChineseDictionary
 import com.google.android.material.navigation.NavigationView
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
@@ -12,15 +14,20 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mDrawerLayout: DrawerLayout
+    private val chineseDictionaryDao = AppDatabase.getInstance(application)!!.chineseDictionaryDao()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        populateDatabase()
         setSupportActionBar(toolbar)
         val navController = findNavController(R.id.nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration(navController.graph, main_drawer_layout)
@@ -47,5 +54,41 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
+    }
+
+    fun populateDatabase(){
+        GlobalScope.launch(Dispatchers.IO) {
+            if (chineseDictionaryDao.getCountDictionary() <= 0) {
+
+                val listOfWordsFromFile = application.assets.open("cedict_ts.u8").bufferedReader().readLines()
+
+                val listOfWordsFormatedForDB = mutableListOf<ChineseDictionary>()
+
+                listOfWordsFromFile.forEach {
+                    var tempString = it
+                    val wordTraditional: String
+                    val wordSimplified: String
+                    val wordPinyin: String
+                    var wordTranslation: String
+
+                    if (tempString.indexOf(" ") > -1 &&
+                        tempString.indexOf("[") > -1 &&
+                        tempString.indexOf("]") > -1 &&
+                        tempString.indexOf("/") > -1 &&
+                        !tempString[0].equals("#")) {
+
+                        wordTraditional = tempString.substring(0, tempString.indexOf(" "))
+                        tempString = tempString.substring(tempString.indexOf(" ") + 1)
+                        wordSimplified = tempString.substring(0, tempString.indexOf(" "))
+                        wordPinyin = tempString.substring(tempString.indexOf("[")+1, tempString.indexOf("]"))
+                        wordTranslation = tempString.substring(tempString.indexOf("/")+1)
+                        wordTranslation = wordTranslation.removeSuffix("/")
+
+                        listOfWordsFormatedForDB.add(ChineseDictionary(null, wordTraditional, wordSimplified, wordPinyin, wordTranslation))
+                    }
+                }
+                chineseDictionaryDao.insertAll(listOfWordsFormatedForDB.toList())
+            }
+        }
     }
 }
