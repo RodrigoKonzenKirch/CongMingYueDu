5094aca6fb4997189af05e51833cab58a4ae1de6
RodrigoKonzenKirch <rodrigokirch@gmail.com>
2019-12-11 16:07:45 -0300

    Added a VocabularyViewModel

---
+++ app/src/main/java/android/practice/com/congmingyuedu/viewmodel/VocabularyViewModel.kt
@@ -0,0 +1,8 @@
+package android.practice.com.congmingyuedu.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class VocabularyViewModel(application: Application) : AndroidViewModel(application) {
+
+}
