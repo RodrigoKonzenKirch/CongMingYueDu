e822b63a2c53c68bbdbf848667d92e559a2ba08e
RodrigoKonzenKirch <rodrigokirch@gmail.com>
2019-12-11 16:02:27 -0300

    Added a Vocabulary details data class

---
+++ app/src/main/java/android/practice/com/congmingyuedu/model/VocabularyDetails.kt
@@ -0,0 +1,9 @@
+package android.practice.com.congmingyuedu.model
+
+data class VocabularyDetails(
+    val simplified: String,
+    val traditional: String,
+    val pinyin: String,
+    val translation: String,
+    val info: String,
+    val examples: String)
